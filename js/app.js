85fc48aef080ba3bacdeaf28f399c086d6a6c923
alessandrodiazrodrigues <alessandrorodrigues@cvc.com.br>
2025-11-11 16:25:24 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@
 
 // =================== CONFIGURAÇÕES GLOBAIS V4.0 ===================
 window.CONFIG = {
-    AUTH_PASSWORD: '1234',
+    AUTH_PASSWORD: '1997',
     ADM_EMAIL: 'cvcalessandro@gmail.com',
-    ADM_PASSWORD: '1234',
+    ADM_PASSWORD: '1997',
     REFRESH_INTERVAL: 240000, // 4 minutos
     QR_TIMEOUT: 120000, // 2 minutos
     HOSPITAIS: {
